3878b4ce628edb833cc77f90cee2bb0582933ad4
Hakan <metinhakan04@hotmail.com>
2023-05-17 19:54:15 +0300

    iletişim sayfası kontrolleri

---
+++ iletisim/iletisim.js
@@ -0,0 +1,32 @@
+function validateForm() {
+  var fname = document.forms["myForm"]["fname"].value;
+  var lname = document.forms["myForm"]["lname"].value;
+  var email = document.forms["myForm"]["email"].value;
+  var iletisim = document.forms["myForm"]["message"].value;
+  var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  var goruntuHtml = parent.document.getElementById("goruntuText");
+
+  if (fname == null || fname == "") {
+    alert("First name can't be blank");
+    return false;
+  } else if (lname == null || lname == "") {
+    alert("Last  name can't be blank");
+    return false;
+  } else if (email == null || email == "") {
+    alert("Email can't be blank");
+    return false;
+  } else if (iletisim == null || iletisim == "") {
+    alert("İletisim kutusu can't be blank");
+    return false;
+  } else if (!email.match(mailFormat)) {
+    alert("geçersiz email");
+    return false;
+  } else {
+    alert(
+      "iletiniz alındı, goruntu sayfasından görüntüleyebilirsinşz" + goruntuHtml
+    );
+
+    goruntuHtml.innerHTML = "aaa";
+    return true;
+  }
+}
